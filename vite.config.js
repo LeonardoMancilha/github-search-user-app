a2a968ffe912170f8a62fe7b97343aab74ad8924
Leonardo Machado <leonardomachado.sjc@gmail.com>
2025-02-14 19:05:28 -0300

    Resolvendo o problema da indexação do site e o reconhecimento do vercel da biblitoteca react-feather

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    optimizeDeps: {
+      include: ['react-feather'],
+    },
+  },
+  resolve: {
+    alias: {
+      'react-feather': 'react-feather/dist/index.js',
+    },
+  },
   plugins: [react()],
 });
